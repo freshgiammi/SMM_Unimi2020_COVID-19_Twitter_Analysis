e60ed5da7cd933ce6757dac549b98d50d6f0dacb
Gianmarco Rengucci <rengucci.gianmarco@gmail.com>
2020-04-30 12:54:10 +0200

    V1: Gather, filter and analyze twitter traffic related to COVID-19 in Italy.
    Signed-off-by: Gianmarco Rengucci <rengucci.gianmarco@gmail.com>

--- twitter_dataanalyzer.py
+++ twitter_dataanalyzer.py
@@ -5,33 +5,76 @@ import pandas as pd
 import random
 import datetime
 
-#Twitter API Initialization
-keys = json.load(open('keys.json'))['twitter']
-bearer_token = twitter.oauth2_dance(consumer_key = keys['consumer_key'], consumer_secret = keys['consumer_secret'])
-twitter_api = twitter.Twitter(auth=twitter.OAuth2(bearer_token = bearer_token))
+from statsmodels.distributions.empirical_distribution import ECDF
+import networkx as nx
+from statsmodels.distributions.empirical_distribution import ECDF
+import numpy as np
+import matplotlib.pyplot as plt
 
 #Load dataset
 #TODO: Merge all datasets, build a base of unique users containing own tweets
-dataset = json.load(open('datasets/Twitter_Dataset_20200424-011336.json'))
+dataset = json.load(open('datasets/dataset.json'))
  
 totaltweets = 0
 totalusers = len(dataset)
 totalrt = 0
+userlist = list()
+realusers = list()
 for t in dataset:
+    userlist.append(list(t.keys())[0])
+    realusers.append(list(t.keys())[0])
     totaltweets = totaltweets + len(t.get('tweets',''))
     for tweet in t.get('tweets',''):
         totalrt = totalrt + len(tweet.get('retweeted_from',''))
+        if len(tweet.get('retweeted_from','')) != 0:
+            for rt in tweet.get('retweeted_from',''):
+                realusers.append(rt)
+            
         #TODO: Check for oldest tweet, compare first one to current time
 
+print("Are there user duplicates in the list? "+str(len(userlist) != len(set(userlist))))
 print("We have a total of "+str(totalusers)+" users and "+str(totaltweets)+" tweets.")
 print("Out of "+str(totaltweets)+" tweets, "+str(totalrt)+" are retweets.")
 print("We calculate around "+str(totaltweets/totalusers)+" tweets per user.")
 
+realusers = list(dict.fromkeys(realusers))
+print("Total unique users (OC+RT) detected: "+str(len(realusers)))
 
+print("Generating edge list...", end=" ")
+edgelist = list()
+for user in dataset:
+    username = list(user.keys())[0]
+    for tweet in user.get('tweets',''):
+        if len(tweet.get('retweeted_from','')) == 0:
+            continue
+        else: 
+            rt_oc_user = list(tweet.get('retweeted_from','').keys())[0]
+            link = (username,rt_oc_user)
+            if username == rt_oc_user:
+                continue
+            if link not in edgelist:
+                edgelist.append(link)
 
-        
-#print("Gathered "+str(len(dataset))+" tweets from a set of "+str(len(unique_users))+" unique users.", sep="")
-#print("Most retweeted tweet is: "+str(dataset[max(max_retweets, key=max_retweets.get)]))
-#print("Most liked tweet is: "+str(dataset[max(max_favorites, key=max_favorites.get)]))
+twitter_d = nx.DiGraph()
+for tuple in edgelist:
+    twitter_d.add_edge(tuple[0], tuple[1])
+print("Edge list generated. Length: "+str(len(edgelist)))
 
+print('Preformatted nodes: {}'.format(twitter_d.order()))
+print("Removing nodes with out-degree less than 10...", end=" ")
+to_be_removed = [x for  x in twitter_d.nodes() if twitter_d.degree(x) <= 9]
+for x in to_be_removed:
+    twitter_d.remove_node(x)
+print("Done.")
+print("Removing isolates...", end=" ")
+to_be_removed = list(nx.isolates(twitter_d))
+for x in to_be_removed:
+    twitter_d.remove_node(x)
+print("Done.")
 
+print('Number of nodes: {}'.format(twitter_d.order()))
+print('Number of links: {}'.format(twitter_d.size()))
+density = nx.density(twitter_d)
+print('Density: {}'.format(density))
+
+nx.write_gexf(twitter_d, "dataset.gexf")
--- twitter_datagatherer.py
+++ twitter_datagatherer.py
@@ -19,31 +19,32 @@ def randomTweetID(search_result):
 #Twitter API Initialization
 keys = json.load(open('keys.json'))['twitter']
 bearer_token = twitter.oauth2_dance(consumer_key = keys['consumer_key'], consumer_secret = keys['consumer_secret'])
-twitter_api = twitter.Twitter(auth=twitter.OAuth2(bearer_token = bearer_token))
+twitter_api = twitter.Twitter(auth=twitter.OAuth2(bearer_token = bearer_token), retry=True)
+
+tweetlist = json.load(open('datasets/dataset.json'))
+#tweetlist = list()
 
 #Query composition and request
-query = '#COVID19'
+query = 'covid OR coronavirus OR covid19 OR covid-19 OR novel OR coronavirus OR covid19italia'
 print("Indexing request 1 ..." ,end=" ")
 response = twitter_api.search.tweets(q=query,count=100, lang='it', tweet_mode='extended')
 print("Request completed.")
 tweets = response['statuses']
 
 #Ask more tweets range(n) times, if they are available. (n+1 return values)
-for i in range(150):
+for i in range(449):
     print("Indexing request",i+2,"..." ,end=" ")
     if 'next_results' in response['search_metadata']:
         max_id = dict([tuple(tok.split('=')) for tok in response['search_metadata']['next_results'][1:].split('&')])['max_id']
-        print("Request completed.")
-        #print(response['search_metadata']['next_results'])
-        response = twitter_api.search.tweets(q=query,count=100, lang='it', tweet_mode='extended', max_id = max_id)
+        response = twitter_api.search.tweets(q=query, count=100, lang='it', tweet_mode='extended', max_id = max_id)
         tweets.extend(response['statuses'])
+        print("Request completed.")
     else:
-        #TODO: RATE LIMITING CASE HANDLING
+        print("Bedrock found. No more tweets to see here!")
         break
 
 # ---------------- JSON PREFORMATTING  ---------------- #
 print("Preformatting tweets into JSON tweetlist...")
-tweetlist = list()
 for t in tweets:
     flag = 0 #Sets flag for multiple tweets in a user.
     #This block defines an ACTIVE USER. 
@@ -69,8 +70,7 @@ for t in tweets:
     tweet['hashtags'] = t.get('entities','')['hashtags']
     tweet['retweets'] = t['retweet_count']
     tweet['favorites'] = t['favorite_count']
-    
-    #TODO: Should we add a field for verified users and narrow down possible hubs?
+
     userinfo['followers'] = t.get('user','')['followers_count']
     userinfo['following'] = t.get('user','')['friends_count']
     userinfo['location'] = t.get('user','')['location']
@@ -78,6 +78,8 @@ for t in tweets:
     #Flows in if the tweet is OC.
     if 'retweeted_status' in t:
         tweet['retweeted_from'] = {t.get('retweeted_status','').get('user','')['screen_name']:t.get('retweeted_status','')['id']}
+    else:
+        tweet['retweeted_from'] = list() #Needed for singlet detection 
 
     #If user ir already in the list, just add the tweet to the list. Otherwise, create a user.
     #TODO: This shit is "a bit less" fugly
@@ -98,7 +100,9 @@ for t in tweets:
 # ---------------- JSON PREFORMATTING  ---------------- #
 print("Preformatting completed. Saving to file...")
 
-with open("datasets/Twitter_Dataset_"+time.strftime("%Y%m%d-%H%M%S")+".json", 'x') as outfile:
+#with open("datasets/Twitter_Dataset_"+time.strftime("%Y%m%d-%H%M%S")+".json", 'x') as outfile:
+#    json.dump(tweetlist, outfile, indent=1)
+with open("datasets/dataset.json", 'w') as outfile:
     json.dump(tweetlist, outfile, indent=1)
 
 print("JSON tweetlist generated. Happy analysis!")
